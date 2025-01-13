347914244d9e769a0ebe392729ed07b0686268b3
hhhhhhjs <1512403978@qq.com>
2025-01-13 14:19:43 +0800

    feat:代码逻辑优化

--- src/views/updateobj.tsx
+++ src/views/updateobj.tsx
@@ -2,40 +2,69 @@ import { useState } from "react";
 
 // 更新对象
 export const Updateobj = () => {
-    const [obj, setobj] = useState({
-        name: '小黄',
-        age: 20,
-        hobby: '吉他'
+  const [obj, setobj] = useState({
+    name: "小黄",
+    age: 20,
+    hobby: "吉他",
+  });
+
+//   const changedataName = (e: any) => {
+//     setobj({
+//       ...obj,
+//       name: e.target.value,
+//     });
+//     console.log(e.target.name, e);
+//   };
+
+//   const changedataAge = (e: any) => {
+//     setobj({
+//       ...obj,
+//       age: Number(e.target.value),
+//     });
+//   };
+
+//   const changedateHobby = (e: any) => {
+//     setobj({
+//       ...obj,
+//       hobby: e.target.value,
+//     });
+//   };
+
+  // 上述逻辑比较繁琐，可以直接在对象中使用变量赋值的方法，只需要一个函数即可
+const changedage = (e:any) => {
+    setobj({
+      ...obj,
+      [e.target.name]: e.target.value,
     })
-
-    const changedataName = (e:React.FormEvent<HTMLInputElement>) => { 
-        setobj({
-            ...obj,
-           name: e.currentTarget.value
-        })
-    }
-
-    const changedataAge = (e:React.FormEvent<HTMLInputElement>) => {
-        setobj({
-            ...obj,
-            age: Number(e.currentTarget.value)
-        })
-    }
-
-    const changedateHobby = (e:React.FormEvent<HTMLInputElement>) => {
-        setobj({
-           ...obj,
-            hobby: e.currentTarget.value
-        })
-    }
-    return (
-        <>
-        <div>
-            <p>今年{obj.name}{obj.age}岁，喜欢{obj.hobby}</p>
-        </div>
-        <input type="text" onInput={changedataName} style={{backgroundColor:'red'}}/>
-        <input type="text" onInput={changedataAge} style={{backgroundColor:'green'}}/>
-        <input type="text" onInput={changedateHobby} style={{backgroundColor:'blue'}}/>
-        </>
-    )
 }
+  return (
+    <>
+      <div>
+        <p>
+          今年{obj.name}
+          {obj.age}岁，喜欢{obj.hobby}
+        </p>
+      </div>
+      <input
+        type="text"
+        name="name"
+        onChange={changedage}
+        style={{ backgroundColor: "red" }}
+      />
+      <input
+        type="text"
+        name="age"
+        onChange={changedage}
+        style={{ backgroundColor: "green" }}
+      />
+      <input
+        type="text"
+        name="hobby"
+        onChange={changedage}
+        style={{ backgroundColor: "blue" }}
+      />
+    </>
+  );
+};
+
+
