297ff94db0bc1db622c88f4934e2c428191b1cea
hhhhhhjs <1512403978@qq.com>
2025-01-16 11:30:25 +0800

    feat:reducer 修改

--- src/views/reducer.tsx
+++ src/views/reducer.tsx
@@ -2,43 +2,50 @@ import { useReducer } from "react";
 
 // 使用 reducer 来实现一个计数器
 
-
 // 定义 reducer
-const reducer = (state: {count:number}, action: { type: string }) => {
+const reducer = (state: { count: number }, action: { type: string }) => {
   switch (action.type) {
     case "add":
-      return { count: state.count + 1};
+      return {
+        ...state,
+        count: state.count + 1,
+      };
     case "sub":
-      return { count: state.count - 1};
+      return {
+        ...state,
+        count: state.count - 1,
+      };
     case "reset":
-      return { count: 0};
+      return {
+        ...state,
+        count: 0,
+      };
     default:
       return state;
   }
-}
+};
 
 export const Counter = () => {
-    const initcount = { count: 0 }
-    const [state, dispatch] = useReducer(reducer, initcount)
-    const handleAdd = () => {
-        dispatch({ type: "add" })
-    }
+  const initcount = { count: 0 };
+  const [state, dispatch] = useReducer(reducer, initcount);
+  const handleAdd = () => {
+    dispatch({ type: "add" });
+  };
 
-    const handleSub = () => {
-        dispatch({ type: "sub" })
-    }
+  const handleSub = () => {
+    dispatch({ type: "sub" });
+  };
 
-    const handleReset = () => {
-        dispatch({ type: "reset" })
-    }
-    return (
-        <div>
-            <h2>计数器</h2>
-            <p>当前计数：{state.count}</p>
-            <button onClick={handleAdd}>+1</button>
-            <button onClick={handleSub}>-1</button>
-            <button onClick={handleReset}>重置</button>
-        </div>
-    )
-}
-
+  const handleReset = () => {
+    dispatch({ type: "reset" });
+  };
+  return (
+    <div>
+      <h2>计数器</h2>
+      <p>当前计数：{state.count}</p>
+      <button onClick={handleAdd}>+1</button>
+      <button onClick={handleSub}>-1</button>
+      <button onClick={handleReset}>重置</button>
+    </div>
+  );
+};
